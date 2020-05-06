caf47654bace691182021793f6b015a857655945
Dikshit Kaushal <47450978+dikshitkaushal@users.noreply.github.com>
2020-05-06 10:37:43 +0530

    rotation and aiming

---
+++ First_Person_Mechanics/Assets/Scripts/first_person_container.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class first_person_container : MonoBehaviour
+{
+    float m_rotationx=0;
+    const float minx = -50f;
+    const float maxx = 50f;
+    const float rotationspeed = 2.0f;
+    player_logic player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponentInParent<player_logic>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_rotationx -= Input.GetAxis("Mouse Y") * rotationspeed;
+        m_rotationx = Mathf.Clamp(m_rotationx, minx, maxx);
+
+    }
+    private void LateUpdate()
+    {
+        if (player)
+        {
+            transform.rotation = Quaternion.Euler(m_rotationx, player.get_rotation_y(), 0);
+        }
+    }
+}
---
+++ First_Person_Mechanics/Assets/Scripts/player_logic.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_logic : MonoBehaviour
+{
+    float m_rotationy;
+    const float m_rotation_speed = 2.0f;
+    float horizontalaxis;
+    float verticalaxis;
+    Vector3 horizontal_move;
+    Vector3 vertical_move;
+    Vector3 totalmove;
+    CharacterController m_charactercontroller;
+    private float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_charactercontroller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalaxis = Input.GetAxis("Horizontal");
+        verticalaxis = Input.GetAxis("Vertical");
+        m_rotationy += Input.GetAxis("Mouse X") * m_rotation_speed;
+        transform.rotation = Quaternion.Euler(0, m_rotationy, 0);
+    }
+    private void FixedUpdate()
+    {
+        horizontal_move = transform.right * horizontalaxis * speed * Time.deltaTime;
+        vertical_move = transform.forward * verticalaxis * speed * Time.deltaTime;
+        totalmove = horizontal_move + vertical_move;
+        if(m_charactercontroller)
+        {
+            m_charactercontroller.Move(totalmove);
+        }
+    }
+    public float get_rotation_y()
+    {
+        return m_rotationy;
+    }
+}
